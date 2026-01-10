39979d7dc2933e973deb7721a2f4b69bed21e2d9
Storybali <sudiarsanachannel@gmail.com>
2026-01-10 12:54:52 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'fishing-spot-v01-auto-update'; // Naikkan versi untuk memicu update ini
+const CACHE_NAME = 'fishing-spot-v07-auto-update'; // Naikkan versi untuk memicu update ini
 const ASSETS = [
     './',
     './index.html',
